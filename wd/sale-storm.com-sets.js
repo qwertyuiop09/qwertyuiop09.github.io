1714f9e29010a19596f9e5897b42ef235becc7cb
John Doe <johndoe@example.com>
2016-07-07 17:33:06 +0300

    new commit

---
+++ wd/sale-storm.com-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='sale-storm.com';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['sale-storm.com'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
