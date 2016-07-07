208b6b7555b87f3aa851673d87c0a816dcc72868
John Doe <johndoe@example.com>
2016-07-07 17:22:10 +0300

    new commit

---
+++ wd/sale-storm.com.js
@@ -0,0 +1,13 @@
+
+	var intepriceIPL = 'sale-storm.inteprice.com';
+	var intepriceCRT = '07.07.2016';
+
+	var IPclhrDataUpdate = {
+
+		"MMFileType": "3",
+		"ExtFormFCStop": {RegExp: '<[^>]+(search|subscribe|password)', Flags: 'i'},
+		"ExtFormBtnFC": '[type=submit], input[type=image], input[type=button], button:not([type=reset])',
+		"ButtonSensitivity":	"0"
+
+
+	}
