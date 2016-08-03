71a4b82d02bc9acbe4ececc8908fa571aafddfe3
John Doe <johndoe@example.com>
2016-08-03 12:56:05 +0300

    new commit

--- wd/callhunter/domains/aib.org.ua.js
+++ wd/callhunter/domains/aib.org.ua.js
@@ -1,4 +1,6 @@
 
+var SSCHunterCallback = function(type,undefined,undefined,undefined,undefined,undefined,Conv_value) { if (type == 200) { console.log ( 'ConvTime exist' ) } else if ((type >= 1)&&(type < 200)) { console.log ( 'ConvCHSnd exist' ) } else if (type == 201) { console.log ( 'ConvРЎlick exist' ) } else if (type == 202) { console.log ( 'ConvSubmit exist' ) } else if (type == 203) { console.log ( 'ConvHit exist' ) } ; if (typeof Conv_value != 'undefined') { console.log ( 'ConvValue exist' ) }   }
+
 
 /*
 		var SSCHunterCallback = function(type) { 
