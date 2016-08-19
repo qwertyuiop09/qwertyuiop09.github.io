b2a1386bf570f49f0046d3553447e1caa8cd9e53
John Doe <johndoe@example.com>
2016-08-19 21:58:07 +0300

    new commit

---
+++ wd/prestige-design.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='prestige-design.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['prestige-design.com.ua','prestige-design.inteprice.com'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
