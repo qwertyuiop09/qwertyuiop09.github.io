5982df279771974a7340e8fca5be11f2360998bc
John Doe <johndoe@example.com>
2016-09-21 21:28:08 +0300

    new commit

--- wd/callhunter/domains/zapchasti-odessa.com.ua.js
+++ wd/callhunter/domains/zapchasti-odessa.com.ua.js
@@ -6,7 +6,7 @@ var intepriceCRT = '18.12.2015';
 
 window.IPCHFEmail = 'aleksey.shmalko@odessa.autotrading.ua';
 
-window.IPINdataexchangeReady = 1;
+window.IPINoneGo = 1;
 
 	(function() {
     	var s = document.createElement('script');
