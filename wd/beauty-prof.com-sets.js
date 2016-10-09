e2110601c556b7565bd291ab354b8fd1e536c76a
John Doe <johndoe@example.com>
2016-10-09 17:58:09 +0300

    new commit

---
+++ wd/beauty-prof.com-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='beauty-prof.com';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['beauty-prof.com'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
