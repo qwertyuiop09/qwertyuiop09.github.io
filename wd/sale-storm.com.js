605bc21b8c638bcd3a221da3ce97cf13a14612dd
John Doe <johndoe@example.com>
2016-10-17 14:45:07 +0300

    new commit

--- wd/sale-storm.com.js
+++ wd/sale-storm.com.js
@@ -40,7 +40,7 @@
 
 
 
-/*
+
 	var setIntervalCountry = setInterval(function() {
 		if (window.jQuery) {
 			clearInterval(setIntervalCountry);
@@ -50,12 +50,6 @@
 			}, "jsonp");
 		}
 	}, 10);
-*/
-
-	window.checkCountry = 'RU';
-	window.jQuery.get("http://ipinfo.io", function(response) {
-		window.checkCountry = response.country;
-	}, "jsonp");
 
 
 
