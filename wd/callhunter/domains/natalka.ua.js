62db0d4ad55d7065cf12f0928c217de18bb9704c
John Doe <johndoe@example.com>
2016-10-21 16:01:11 +0300

    new commit

--- wd/callhunter/domains/natalka.ua.js
+++ wd/callhunter/domains/natalka.ua.js
@@ -2,8 +2,6 @@
 var SSCHunterCallback = function(type,undefined,undefined,undefined,undefined,undefined,Conv_value) { if (type == 200) { console.log ( 'ConvTime exist' ) } else if ((type >= 1)&&(type < 200)) { console.log ( 'ConvCHSnd exist' ) } else if (type == 201) { console.log ( 'ConvСlick exist' ) } else if (type == 202) { console.log ( 'ConvSubmit exist' ) } else if (type == 203) { console.log ( 'ConvHit exist' ) } else if (type == 204) { console.log ( 'ConvJivo exist' ) } else if (type == 205) { console.log ( 'CallWidget exist' ) } ; if (typeof Conv_value != 'undefined') { console.log ( 'ConvValue exist: ' + Conv_value ) }   }
 
 
-
-//без предоплаты, логин менеджера
 var intepriceIPL = 'natalka.inteprice.com';
 var intepriceCRT = '15.10.2016';
 
