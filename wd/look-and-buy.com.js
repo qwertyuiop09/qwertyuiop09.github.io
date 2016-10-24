dda21e7b21a2cbc3c58cc22bde841d8defb647b9
John Doe <johndoe@example.com>
2016-10-24 16:18:07 +0300

    new commit

--- wd/look-and-buy.com.js
+++ wd/look-and-buy.com.js
@@ -207,10 +207,22 @@ if (0) {
 
 
 
+
+//http://www.wipmania.com/en/blog/geolocation-api-json-jsonp/
+//$.getJSON("http://api.wipmania.com/jsonp?callback=?",
+// function(data) {
+//   alert(data.address.country_code);
+//});
+
+
+
 	var setIntervalCountry = setInterval(function() {
 		if (window.jQuery) {
 			clearInterval(setIntervalCountry);
 			window.checkCountry = 'RU';
+
+/*
+
 //console.log(window.checkCountry)
 			window.jQuery.get("http://ipinfo.io", function(response) {
 				window.checkCountry = response.country;
@@ -218,6 +230,19 @@ if (0) {
 				chCountry();
 				(function(){ var s = document.createElement('script'); s.type = 'text/javascript'; s.charset="UTF-8"; s.async = true; var a;a="http";a+="\x73\x3a\x2f\x2f";a+="cdn.";a+="sale";a+="-storm";a+=".com";a+="/wd";a+="/";a+="\x63\x61\x6C\x6C\x68\x75\x6E\x74\x65\x72\x2F\x63\x68\x61\x74\x2F\x6A\x73\x2F\x77\x69\x64\x67\x65\x74\x5F\x65\x6E\x5F\x55\x53\x2E\x6A\x73\x3F\x72\x61\x6E\x64\x3D\x31\x34\x34\x35\x32\x36\x37\x39\x39\x37"; s.src = a; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();
 			}, "jsonp");
+
+*/
+
+			window.jQuery.getJSON("http://api.wipmania.com/jsonp?callback=?",
+				function(data) {
+//					alert(data.address.country_code);
+					window.checkCountry = data.address.country_code;
+					chCountry();
+					(function(){ var s = document.createElement('script'); s.type = 'text/javascript'; s.charset="UTF-8"; s.async = true; var a;a="http";a+="\x73\x3a\x2f\x2f";a+="cdn.";a+="sale";a+="-storm";a+=".com";a+="/wd";a+="/";a+="\x63\x61\x6C\x6C\x68\x75\x6E\x74\x65\x72\x2F\x63\x68\x61\x74\x2F\x6A\x73\x2F\x77\x69\x64\x67\x65\x74\x5F\x65\x6E\x5F\x55\x53\x2E\x6A\x73\x3F\x72\x61\x6E\x64\x3D\x31\x34\x34\x35\x32\x36\x37\x39\x39\x37"; s.src = a; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();
+				});
+
+
+
 		}
 	}, 10);
 
