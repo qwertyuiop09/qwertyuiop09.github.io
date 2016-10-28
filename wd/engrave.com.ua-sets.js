5c7dead1d79132d582904557503ea4da9540d7c5
John Doe <johndoe@example.com>
2016-10-28 14:58:09 +0300

    new commit

---
+++ wd/engrave.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='engrave.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['engrave.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
