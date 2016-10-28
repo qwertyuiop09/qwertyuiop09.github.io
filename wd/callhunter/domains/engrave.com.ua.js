c92340f117f0f62603d0de4302ad3bb509ef0e1b
John Doe <johndoe@example.com>
2016-10-28 18:10:12 +0300

    new commit

--- wd/callhunter/domains/engrave.com.ua.js
+++ wd/callhunter/domains/engrave.com.ua.js
@@ -150,7 +150,7 @@ if (0) {
 
 		"MMFileType": "2",
 		"ExtFormFCStop": {RegExp: '<[^>]+(search|subscribe|password)', Flags: 'i'},
-		"ExtFormBtnFC": '[type=submit], input[type=image], input[type=button], button:not([type=reset])',
+//		"ExtFormBtnFC": '[type=submit], input[type=image], input[type=button], button:not([type=reset])',
 		"ButtonSensitivity":	"0",
 		"ButtonTransform": 3,
 		"ButtonYMobile": 80,
