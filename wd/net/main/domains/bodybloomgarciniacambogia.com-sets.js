5f4ec7d93ab8dd21aa499e5fd1de70db4a54263e
John Doe <johndoe@example.com>
2016-11-01 20:26:07 +0200

    new commit

---
+++ wd/net/main/domains/bodybloomgarciniacambogia.com-sets.js
@@ -0,0 +1,18 @@
+
+
+//обязательная строка, без нее не будет работать конверсия
+	Ipadsmaincpa0916.IpCpA = Ipadsmaincpa0916.strip_www(location.hostname);
+
+//все остальные строки тоже обязательные, требуют заполнения и тщательной проверки (цена валюта аккаунт метка)
+
+	Ipadsmaincpa0916.Conv =  {
+	google_conversion_id: 870936332,
+	google_conversion_language: "en",
+	google_conversion_format: "3",
+	google_conversion_color: "ffffff",
+	google_conversion_label: "KGCNCN-JvmsQjN6lnwM",
+	google_conversion_value: 60.00,
+	google_conversion_currency: "USD",
+	google_remarketing_only: false
+		}
+
