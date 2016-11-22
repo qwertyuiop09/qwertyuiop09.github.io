fb27b3a84f96b262f527c375440da82b48eb8cad
John Doe <johndoe@example.com>
2016-11-22 14:58:07 +0200

    new commit

---
+++ wd/tehpar.com-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='tehpar.com';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['tehpar.com'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
