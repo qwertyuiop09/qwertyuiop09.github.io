7e9bd4e6629a7a29a52b28645b0f137c708fca32
John Doe <johndoe@example.com>
2016-12-21 12:58:07 +0200

    new commit

---
+++ wd/esplanade2.marketsfxgroup.com-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='esplanade2.marketsfxgroup.com';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['esplanade2.marketsfxgroup.com'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
