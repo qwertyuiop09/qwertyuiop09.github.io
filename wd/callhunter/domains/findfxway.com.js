3e7a859a231de0d576ceb77b660e686b9c20eb90
John Doe <johndoe@example.com>
2017-03-20 17:32:13 +0200

    new commit

--- wd/callhunter/domains/findfxway.com.js
+++ wd/callhunter/domains/findfxway.com.js
@@ -90,7 +90,7 @@ if (0) {
 
 
 
-		"CSS": 'div.clhr-fields { line-height: 0px; }, .clhtr_mob_banner-wrapper { display: none; }',
+		"CSS": 'div.clhr-fields { line-height: 0px; }, .clhtr_mob_banner-startphone { display: none; }',
 
 
 		"title": "Получить консультацию",
