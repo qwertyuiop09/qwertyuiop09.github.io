e811bf2c794b60e63d22806a48b428d86aecf7f0
John Doe <johndoe@example.com>
2017-03-20 13:58:12 +0200

    new commit

---
+++ wd/findfxway.com-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='findfxway.com';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['findfxway.com'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
