b42b79996ec79ab23d0ca2f4de9e765aaf040905
John Doe <johndoe@example.com>
2017-04-06 16:27:11 +0300

    new commit

--- wd/callhunter/domains/papyrus.kiev.ua.js
+++ wd/callhunter/domains/papyrus.kiev.ua.js
@@ -96,36 +96,39 @@ if (0) {
 //	"workDays": "Mon, Tue, Wed, Thu, Fri",
 
 
-/*
+
+
+
 
 	"AdwConvRemark": {
-	google_conversion_id: 881826282,
+	google_conversion_id: 930826837,
 	google_custom_params: window.google_tag_params,
 	google_remarketing_only: true
 		},
 
 
 	"AdwConvTime": {
-	google_conversion_id: 881826282,
+	google_conversion_id: 930826837,
 	google_conversion_language: "en",
 	google_conversion_format: "3",
 	google_conversion_color: "ffffff",
-	google_conversion_label: "69LkCIeZ7mgQ6rO-pAM",
+	google_conversion_label: "syVJCLuR_28Q1ZTtuwM",
 	google_remarketing_only: false
 		},
 
 
 	"AdwConvCHSnd": {
-	google_conversion_id: 881826282,
+	google_conversion_id: 930826837,
 	google_conversion_language: "en",
 	google_conversion_format: "3",
 	google_conversion_color: "ffffff",
-	google_conversion_label: "7ElWCKKZ7mgQ6rO-pAM",
+	google_conversion_label: "JGtsCPCQ_28Q1ZTtuwM",
 	google_remarketing_only: false
 		},
 
 
-*/
+
+
 
 		"MMFileType": "2",
 //		"ExtFormFCStop": {RegExp: '<[^>]+(search|subscribe|password)', Flags: 'i'},
