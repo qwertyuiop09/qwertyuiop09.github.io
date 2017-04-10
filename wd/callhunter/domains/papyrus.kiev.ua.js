1a1e8ab03a509fae16d47ece72eede845022aeaa
John Doe <johndoe@example.com>
2017-04-10 19:05:14 +0300

    new commit

--- wd/callhunter/domains/papyrus.kiev.ua.js
+++ wd/callhunter/domains/papyrus.kiev.ua.js
@@ -85,7 +85,18 @@ if (0) {
 	var IPclhrDataUpdate = {
 
 
-	"fields": "+Введите ваш телефон(+380)*, -Тема звонка(необязательно)",
+//	"fields": "+Введите ваш телефон(+380)*, -Тема звонка(необязательно)",
+
+		"title": "Получить консультацию",
+		"titleworktime": "Мы свяжемся с Вами через несколько минут",
+		"titlenight": "Мы свяжемся с Вами в рабочее время",
+		"button": "Жду ответ!",
+		"callTime": "0",
+		"fields": "+Введите ваш телефон(+380)*, Введите ваш Email*, -Тема (Ваш вопрос)*",
+		"emptyPhone": "+380",
+
+
+
 
 	// оформление кнопки (green или gray)
 	"ButtonColor":	"gray",
@@ -154,8 +165,8 @@ if (0) {
 	"consultantSettings": {
 	        phone_field: 1,
 	        phone_required: 1,
-	        email_field: 0,
-	        email_required: 0,
+	        email_field: 1,
+	        email_required: 1,
 	        name_field: 1,
 	        name_required: 0,
 	        fields_forced: 1,
