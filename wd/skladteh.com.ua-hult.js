88b74533274e73142ac1ccc615c49d5f6ca6f821
John Doe <johndoe@example.com>
2017-04-27 18:23:09 +0300

    new commit

---
+++ wd/skladteh.com.ua-hult.js
@@ -0,0 +1,7 @@
+function checkloadIPactive(a){a.intepriceCallHunterActive=!1;if(a.jqIPAcode&&(a.jqIPAcode("#clhtr_mob_banner-startphone").hide(),a.jqIPAcode("#clhtr_phone_div").hide(),!a.intepriceCHOKS)){var b=a.IPclhrDataUpdate&&Number(IPclhrDataUpdate.consultantOn)||a.IPclhrData&&Number(IPclhrData.consultantOn);b&&(a.intepriceCHOKS=1,1==b?a.jqIPAcode("<style>").prop("type","text/css").html("div#supportTrigger, div#supportTriggerMobile { display: none!important; visibility: hidden!important; }").appendTo("head"):2==b?a.jqIPAcode("<style>").prop("type","text/css").html("div#sh_button { display: none!important; visibility: hidden!important; }").appendTo("head"):3!=b&&4!=b||a.jqIPAcode("<style>").prop("type","text/css").html("div#jivo_chat_widget, div#jivo-iframe-container { display: none!important; visibility: hidden!important; }").appendTo("head"))}setTimeout(function(){checkloadIPactive(a)},2E3)}setTimeout(function(){checkloadIPactive(window)},700);
+
+//(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+//new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+//j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+//'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+//})(window,document,'script','dataLayer','GTM-KNGB49');
