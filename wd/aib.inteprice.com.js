a2a6e1c41932eaec6f38af647aa32a174aa603b1
John Doe <johndoe@example.com>
2017-09-01 16:34:13 +0300

    new commit

--- wd/aib.inteprice.com.js
+++ wd/aib.inteprice.com.js
@@ -21,6 +21,20 @@ alert(jQuery.fn.jquery);
 
 
 
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+
+  ga('create', 'UA-50940775-1', { 'cookieDomain': 'auto', 'siteSpeedSampleRate': 50 });
+
+  ga('require', 'linkid', 'linkid.js');
+  ga('require', 'displayfeatures');
+  ga('send', 'pageview');
+
+
+/*
 
 //-- Google Analytics counter --
 
@@ -54,6 +68,9 @@ alert(jQuery.fn.jquery);
 
 //-- /Google Analytics counter --
 
+
+
+*/
 
 
 
