de38c8357861972725257e90a863688d09fba1d9
John Doe <johndoe@example.com>
2017-12-23 11:32:12 +0200

    new commit

---
+++ wd/home.natura-mebel.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='home.natura-mebel.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['home.natura-mebel.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
---
+++ wd/natura-mebel.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='natura-mebel.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['natura-mebel.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
