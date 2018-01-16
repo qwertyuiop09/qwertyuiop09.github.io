8b8c7b60182825a6541098e819a1fe273aa55e5e
John Doe <johndoe@example.com>
2018-01-16 16:54:10 +0200

    new commit

---
+++ wd/жалюзи.liart-okna.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='жалюзи.liart-okna.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['жалюзи.liart-okna.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
