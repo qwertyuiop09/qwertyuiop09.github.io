4555ea9d0bec567a2dc3f023b187a001ff9fd8e9
John Doe <johndoe@example.com>
2018-01-17 00:43:11 +0200

    new commit

---
+++ wd/xn--80aleen8i.liart-okna.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='xn--80aleen8i.liart-okna.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['xn--80aleen8i.liart-okna.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
