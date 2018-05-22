af7747c7fb62c66b146333b298ba7b9049d01c0f
John Doe <johndoe@example.com>
2018-05-22 20:32:11 +0300

    new commit

---
+++ wd/kamalayastudio.me-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='kamalayastudio.me';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['kamalayastudio.me'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
