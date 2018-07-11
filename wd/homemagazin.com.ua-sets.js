3690870bcde9b603528a804dde52f88b716143e1
John Doe <johndoe@example.com>
2018-07-11 11:32:14 +0300

    new commit

---
+++ wd/homemagazin.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='homemagazin.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['homemagazin.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
