893b64c70db232251e0699f7111396a56d136882
John Doe <johndoe@example.com>
2018-08-10 02:21:10 +0300

    new commit

--- wd/callhunter/domains/accumoil.od.ua.js
+++ wd/callhunter/domains/accumoil.od.ua.js
@@ -49,6 +49,9 @@ var IPchatConfig = {
 
 if (window.jQuery) {
 
+	//блокируем сумму в корзине
+	window.jQuery("<style>").prop("type", "text/css").html(".article > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) { display: none!important; visibility: hidden!important; }").appendTo("head");
+
 	//ваш заказ
 	var YourOrder = window.jQuery('.card')[0];
 	if (YourOrder) {
