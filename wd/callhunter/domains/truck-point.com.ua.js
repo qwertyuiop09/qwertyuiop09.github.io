63f9b6cb66ae8163a06fdfca80303f1ad0578411
John Doe <johndoe@example.com>
2018-08-29 19:59:10 +0300

    new commit

--- wd/callhunter/domains/truck-point.com.ua.js
+++ wd/callhunter/domains/truck-point.com.ua.js
@@ -182,8 +182,8 @@ var IPchatConfig = {
 	"consultantSettings": {
 	        phone_field: 1,
 	        phone_required: 1,
-	        email_field: 1,
-	        email_required: 1,
+	        email_field: 0,
+	        email_required: 0,
 	        name_field: 1,
 	        name_required: 0,
 	        fields_forced: 1,
