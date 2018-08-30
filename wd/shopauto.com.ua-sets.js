a3607ea437f507e7532e652e547ec97061c2b189
John Doe <johndoe@example.com>
2018-08-30 03:32:21 +0300

    new commit

---
+++ wd/shopauto.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='shopauto.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['shopauto.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
