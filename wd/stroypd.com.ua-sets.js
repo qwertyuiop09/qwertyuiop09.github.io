5c02bada48335701ed8a458c5b8a6b95f15f64d0
John Doe <johndoe@example.com>
2018-09-05 11:32:16 +0300

    new commit

---
+++ wd/stroypd.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='stroypd.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['stroypd.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
