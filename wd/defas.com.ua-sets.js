fe6e30baad28ddba77b03e6a59d511af94129120
John Doe <johndoe@example.com>
2018-10-01 15:52:16 +0300

    new commit

---
+++ wd/defas.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='defas.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['defas.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
