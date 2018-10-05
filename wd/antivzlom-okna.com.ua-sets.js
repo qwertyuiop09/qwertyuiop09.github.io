31a89b5488e323367bea672d8bd93524ab968ff1
John Doe <johndoe@example.com>
2018-10-05 19:19:12 +0300

    new commit

---
+++ wd/antivzlom-okna.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='antivzlom-okna.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['antivzlom-okna.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
