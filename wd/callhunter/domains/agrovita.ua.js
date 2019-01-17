d7cc83b4a110ca0fda81b491add9026d887d7416
John Doe <johndoe@example.com>
2019-01-17 12:21:15 +0200

    new commit

--- wd/callhunter/domains/agrovita.ua.js
+++ wd/callhunter/domains/agrovita.ua.js
@@ -133,7 +133,7 @@ var IPchatConfig = {
 
 		"MMFileType": "2",
 		"ExtFormFCStop": {RegExp: '<[^>]+(search|subscribe|password)', Flags: 'i'},
-//		"ExtFormFCOnly": {RegExp: '<[^>]+("[ ]*Оформить[ ]+заказ[ ]*"|\'[ ]*Оформить[ ]+заказ[ ]*\')', Flags: 'i'},
+		"ExtFormFCOnly": {RegExp: '<[^>]+("[ ]*Відправити[ ]+замовлення\![ ]*")', Flags: 'i'},
 		"ExtFormBtnFC": '[type=submit], input[type=image], input[type=button], button:not([type=reset])',
 		"ButtonSensitivity":	"0",
 		"ButtonTransform": 3,
