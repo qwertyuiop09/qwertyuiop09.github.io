15eb7cae7ab97651d92bfc9b492c5093883f99a8
John Doe <johndoe@example.com>
2019-01-31 15:52:13 +0200

    new commit

---
+++ wd/digital-protection.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='digital-protection.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['digital-protection.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
