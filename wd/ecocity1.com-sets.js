7dc5e76434157d98366f7127a495d802f6be6273
John Doe <johndoe@example.com>
2019-05-02 14:52:19 +0300

    new commit

---
+++ wd/ecocity1.com-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='ecocity1.com';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['ecocity1.com'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
