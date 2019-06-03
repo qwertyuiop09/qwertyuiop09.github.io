dd3cf08c5a50a373dc6637407bd4858f79d058cd
John Doe <johndoe@example.com>
2019-06-03 23:28:27 +0300

    new commit

---
+++ wd/files/nina_dbase.js
@@ -0,0 +1,1 @@
+{"Num":2,"base":{"http://www.stikon.od.ua":[1,"03.06.2019",1,"http://www.stikon.od.ua",undefined],"http://www.kandinsky-residence.com.ua":[2,"03.06.2019",1,"http://www.kandinsky-residence.com.ua",undefined]}}
