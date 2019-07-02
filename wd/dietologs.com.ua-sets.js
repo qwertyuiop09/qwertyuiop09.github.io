764b8d3f3b3d4b82947c7cd10e717283d8fbb6b7
John Doe <johndoe@example.com>
2019-07-02 14:52:16 +0300

    new commit

---
+++ wd/dietologs.com.ua-sets.js
@@ -0,0 +1,16 @@
+
+var IpAid='dietologs.com.ua';
+
+
+var intepriceApermissions = {
+	"ApprovedDomains":	['dietologs.com.ua'],
+	"CallHunter":		true
+};
+
+
+if (typeof window.intepriceCallHunterIni === "undefined") {
+	window.intepriceCallHunterIni='config.js';
+}
+
+
+if (typeof intepriceCallHunterInit == 'function') { intepriceCallHunterInit(); }
