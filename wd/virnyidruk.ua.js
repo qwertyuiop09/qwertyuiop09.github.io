49d08bea3fc8388907b1d61005b11eeb84334b09
John Doe <johndoe@example.com>
2019-12-06 16:04:17 +0200

    new commit

--- wd/virnyidruk.ua.js
+++ wd/virnyidruk.ua.js
@@ -2,28 +2,35 @@ var SSCHunterGa=function(b,c){try{var a=window[window.GoogleAnalyticsObject];a(f
 
 var pagecur=window.location.href.toString().split(window.location.host)[1];
 if (pagecur.indexOf('wrk=12') >= 0) {
+
+	function docready(f){/in/.test(document.readyState)?setTimeout(docready,9,f):f()}
+
+	docready(function(){
+
 //передаем в аналитикс Заинтересованность 1с
-	try {
-		if(typeof SSCHunterGa=="function")SSCHunterGa("Client Active","ss_send_active");
-		console.log('SSCHunterGa Ok');
-	} catch (e) {
-		console.log('SSCHunterGa Error, name: ' + e.name + ", message: " + e.message);
-	}
+		try {
+			if(typeof SSCHunterGa=="function")SSCHunterGa("Client Active","ss_send_active");
+			console.log('SSCHunterGa Ok');
+		} catch (e) {
+			console.log('SSCHunterGa Error, name: ' + e.name + ", message: " + e.message);
+		}
 //передаем в ads Заинтересованность 2с
-	try {
-		var AdwConvTime = {
-		google_conversion_id: 697356077,
-		google_conversion_language: "en",
-		google_conversion_format: "3",
-		google_conversion_color: "ffffff",
-		google_conversion_label: "_mXbCKzbuLYBEK2ew8wC",
-		google_remarketing_only: false
-			};
-		console.log(typeof(window['google_trackConversion']));
-		SSCHunterAdwordstrack(AdwConvTime);
-		console.log('SSCHunterAdwordstrack Ok');
-	} catch (e) {
-		console.log('SSCHunterAdwordstrack Error, name: ' + e.name + ", message: " + e.message);
-	}
+		try {
+			var AdwConvTime = {
+			google_conversion_id: 697356077,
+			google_conversion_language: "en",
+			google_conversion_format: "3",
+			google_conversion_color: "ffffff",
+			google_conversion_label: "_mXbCKzbuLYBEK2ew8wC",
+			google_remarketing_only: false
+				};
+			console.log(typeof(window['google_trackConversion']));
+			SSCHunterAdwordstrack(AdwConvTime);
+			console.log('SSCHunterAdwordstrack Ok');
+		} catch (e) {
+			console.log('SSCHunterAdwordstrack Error, name: ' + e.name + ", message: " + e.message);
+		}
+	});     
+
 
 }
