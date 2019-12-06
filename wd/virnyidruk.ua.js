eaa8674e3d90e6965fc3aaa27723ec6134ac46d8
John Doe <johndoe@example.com>
2019-12-06 15:24:19 +0200

    new commit

--- wd/virnyidruk.ua.js
+++ wd/virnyidruk.ua.js
@@ -1,5 +1,23 @@
 var SSCHunterGa=function(b,c){try{var a=window[window.GoogleAnalyticsObject];a(function(){a.getAll().forEach(function(a){a.send("event",b,c)})})}catch(d){}};
+
 var pagecur=window.location.href.toString().split(window.location.host)[1];
 if (pagecur.indexOf('wrk=12') >= 0) {
-	if(typeof SSCHunterGa=="function")SSCHunterGa("Client Active","ss_send_active");
+//передаем в аналитикс Заинтересованность 1с
+	try {
+		if(typeof SSCHunterGa=="function")SSCHunterGa("Client Active","ss_send_active");
+	} catch (e) { }
+//передаем в ads Заинтересованность 2с
+	try {
+		var AdwConvTime = {
+		google_conversion_id: 697356077,
+		google_conversion_language: "en",
+		google_conversion_format: "3",
+		google_conversion_color: "ffffff",
+		google_conversion_label: "_mXbCKzbuLYBEK2ew8wC",
+		google_remarketing_only: false
+			};
+		SSCHunterAdwordstrack(AdwConvTime);
+	} catch (e) { }
+
 }
+
