ec70d95df663d795e86b78a474f1783086b8b7d3
John Doe <johndoe@example.com>
2019-12-08 09:51:51 +0200

    new commit

--- wd/callhunter/domains/virnyidruk.ua.js
+++ wd/callhunter/domains/virnyidruk.ua.js
@@ -3,7 +3,8 @@ var SSCHunterCallback=function(type,undefined1,undefined2,undefined3,undefined4,
 
 
 /*
-есть в основной директории такой код, комментируем здесь
+есть в основной директории такой код, закомментировано
+//var SSCHunterGa=function(b,c){try{var a=window[window.GoogleAnalyticsObject];a(function(){a.getAll().forEach(function(a){a.send("event",b,c)})})}catch(d){}};
 var pagecur=window.location.href.toString().split(window.location.host)[1];
 if (pagecur.indexOf('wrk=12') >= 0) {
 
