dc3420b9f938bc739fa97f2044844b636b32063b
John Doe <johndoe@example.com>
2020-05-22 15:32:18 +0300

    new commit

--- wd/callhunter/domains/virnyidruk.ua.js
+++ wd/callhunter/domains/virnyidruk.ua.js
@@ -7,7 +7,6 @@ var SSCHunterCallback=function(type,undefined1,undefined2,undefined3,undefined4,
 //var SSCHunterGa=function(b,c){try{var a=window[window.GoogleAnalyticsObject];a(function(){a.getAll().forEach(function(a){a.send("event",b,c)})})}catch(d){}};
 var pagecur=window.location.href.toString().split(window.location.host)[1];
 if (pagecur.indexOf('wrk=12') >= 0) {
-//if (1) {
 
 	function docready(f){/in/.test(document.readyState)?setTimeout(docready,9,f):f()}
 
