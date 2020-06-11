fa3b97c4a9042fa8ba76cc871b959c3cdd9308dd
John Doe <johndoe@example.com>
2020-06-11 14:10:09 +0000

    new commit

--- wd/callhunter/domains/jambo.ua.js
+++ wd/callhunter/domains/jambo.ua.js
@@ -246,6 +246,26 @@ var IPchatConfig = {
 
 
 
+		function recreateNode(el, withChildren) {
+		  if (withChildren) {
+		    el.parentNode.replaceChild(el.cloneNode(true), el);
+		  } else {
+		    var newEl = el.cloneNode(false);
+		    while (el.hasChildNodes()) newEl.appendChild(el.firstChild);
+		    el.parentNode.replaceChild(newEl, el);
+		  }
+		}
+
+
+//это удаление всех предыдущих js-обработчиков
+		window.jqIPAcode('#request-call').each(function() {
+			var buttonCurr=window.jqIPAcode(this)[0];
+			recreateNode(buttonCurr, true);
+		});
+
+
+
+
 
 	}
 
