151aa02abc9e9c0832d08635d047ff2a40c0b4c8
John Doe <johndoe@example.com>
2020-08-13 06:24:07 +0000

    new commit

---
+++ wd/zukanlab.com.js
@@ -0,0 +1,40 @@
+var pagecur=window.location.href.toString().split(window.location.host)[1];
+if (pagecur.indexOf('wrk=12') >= 0) {
+
+	function docready(f){/in/.test(document.readyState)?setTimeout(docready,9,f):f()}
+
+//без docready не всегда передаются конверсии в аналитикс, и не работает SSCHunterAdwordstrack (так как не успевает до того подгрузится SSCHunterAdwordstrack())
+	docready(function(){
+
+//передаем в ads Запрос цены
+		try {
+
+			if (Math.random() < 0.5) {
+			//if (1) {
+
+				setTimeout(function() {
+
+					var AdwConvTime = {
+					google_conversion_id: 612145020,
+					google_conversion_language: "en",
+					google_conversion_format: "3",
+					google_conversion_color: "ffffff",
+					google_conversion_label: "VglsCIaz-doBEPyu8qMC",
+					google_remarketing_only: false
+						};
+					console.log(typeof(window['google_trackConversion']));
+					SSCHunterAdwordstrack(AdwConvTime);
+					console.log('SSCHunterAdwordstrack Ok');
+
+
+				}, 1000);
+
+
+			}
+		} catch (e) {
+			console.log('SSCHunterAdwordstrack Error, name: ' + e.name + ", message: " + e.message);
+		}
+	});     
+
+
+}
