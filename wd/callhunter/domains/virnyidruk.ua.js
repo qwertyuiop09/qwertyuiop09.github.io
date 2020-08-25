59b52cedb7cd2b0d9f089450365bacaadf4a3082
John Doe <johndoe@example.com>
2020-08-25 08:44:08 +0000

    new commit

--- wd/callhunter/domains/virnyidruk.ua.js
+++ wd/callhunter/domains/virnyidruk.ua.js
@@ -58,6 +58,97 @@ if (pagecur.indexOf('wrk=12') >= 0) {
 
 
 
+
+
+
+//код для кампании-генератора
+if (typeof SSCHunterGa != 'function')
+	var SSCHunterGa=function(b,c){try{var a=window[window.GoogleAnalyticsObject];a(function(){a.getAll().forEach(function(a){a.send("event",b,c)})})}catch(d){}};
+var pagecur=window.location.href.toString().split(window.location.host)[1];
+if (pagecur.indexOf('wd=11') >= 0) {
+
+	function docready(f){/in/.test(document.readyState)?setTimeout(docready,9,f):f()}
+
+//без docready не всегда передаются конверсии в аналитикс, и не работает SSCHunterAdwordstrack (так как не успевает до того подгрузится SSCHunterAdwordstrack())
+	docready(function(){
+
+//передаем в аналитикс Заинтересованность 3с
+		try {
+			if(typeof SSCHunterGa=="function")SSCHunterGa("Client Active","send_active2");
+			console.log('SSCHunterGa Ok');
+		} catch (e) {
+			console.log('SSCHunterGa Error, name: ' + e.name + ", message: " + e.message);
+		}
+
+//передаем в ads Запрос цены
+		try {
+
+
+
+			//if (Math.random() < 0.9) {
+			if (1) {
+
+				setTimeout(function() {
+
+					var AdwConvTime = {
+					google_conversion_id: 697356077,
+					google_conversion_language: "en",
+					google_conversion_format: "3",
+					google_conversion_color: "ffffff",
+					google_conversion_label: "D1PSCNjnwdwBEK2ew8wC",
+					google_remarketing_only: false
+						};
+					console.log(typeof(window['google_trackConversion']));
+					SSCHunterAdwordstrack(AdwConvTime);
+					console.log('SSCHunterAdwordstrack Ok');
+
+
+				}, 1000);
+
+
+			}
+
+		}
+
+
+
+//передаем в ads Начало оформления покупки
+		try {
+
+			if (Math.random() < 0.8) {
+			//if (1) {
+
+				setTimeout(function() {
+
+					var AdwConvTime = {
+					google_conversion_id: 697356077,
+					google_conversion_language: "en",
+					google_conversion_format: "3",
+					google_conversion_color: "ffffff",
+					google_conversion_label: "DvtQCKKkuNwBEK2ew8wC",
+					google_remarketing_only: false
+						};
+					console.log(typeof(window['google_trackConversion']));
+					SSCHunterAdwordstrack(AdwConvTime);
+					console.log('SSCHunterAdwordstrack Ok');
+
+
+				}, 1000);
+
+
+			}
+
+
+		} catch (e) {
+			console.log('SSCHunterAdwordstrack Error, name: ' + e.name + ", message: " + e.message);
+		}
+	});     
+
+
+}
+
+
+
 function setSS_google_tag_params(arrayhash) {
 //установить window.google_tag_params из arrayhash (массива хешей)
 
