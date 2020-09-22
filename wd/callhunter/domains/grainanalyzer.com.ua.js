aee86732d5d7d1937ba5a927619441d7c78de716
John Doe <johndoe@example.com>
2020-09-22 15:29:07 +0000

    new commit

--- wd/callhunter/domains/grainanalyzer.com.ua.js
+++ wd/callhunter/domains/grainanalyzer.com.ua.js
@@ -66,14 +66,14 @@ var IPchatConfig = {
 
 
 
-/*
+
 
 		"AdwConvRemark": {
 		google_conversion_id: 615887396,
 		google_custom_params: window.google_tag_params,
 		google_remarketing_only: true
 			},
-*/
+
 
 
 
